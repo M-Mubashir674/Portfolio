bcd6c142a432449e06c4ff12616206614174c2da
M-Mubashir674 <mmubashir.bcss19@iba-suk.edu.pk>
2023-01-12 16:34:41 +0500

    adding some more technologies

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -14,14 +14,19 @@ import {
   SiNextdotjs,
   SiWeb3Dotjs,
   SiJava,
-  SiMysql
+  SiMysql,
+  SiFlutter,
+  SiFirebase
 } from "react-icons/si";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiWeb3Dotjs />
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNextdotjs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
@@ -30,25 +35,25 @@ function Techstack() {
         <DiNodejs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
+        <SiFlutter />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSolidity/>
+        <SiWeb3Dotjs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiJava />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
         <SiMysql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFirebase />
       </Col>
     </Row>
   );
