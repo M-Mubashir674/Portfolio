10273bf77d1843f3b473e7fe4ebbb6db0c2767a5
M-Mubashir674 <mmubashir.bcss19@iba-suk.edu.pk>
2023-01-12 16:46:02 +0500

    some updates in tools and tech section

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
 import {
   DiJavascript1,
   DiReact,
   DiNodejs,
-  DiMongodb,
-  DiGit
+  DiMongodb
 } from "react-icons/di";
 import {
-  SiPytorch,
-  SiSolidity,
   SiNextdotjs,
   SiWeb3Dotjs,
   SiJava,
@@ -22,37 +18,34 @@ import {
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" title="React.js">
         <DiReact />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" title="Next.js">
         <SiNextdotjs />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" title="Javascript">
         <DiJavascript1 />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" title="Node.js">
         <DiNodejs />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" title="Flutter">
         <SiFlutter />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" title="Web3.js">
         <SiWeb3Dotjs />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" title="Java">
         <SiJava />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
+      <Col xs={4} md={2} className="tech-icons" title="MongoDB">
+        <DiMongodb/>
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" title="MySQL">
         <SiMysql />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" title="Firebase">
         <SiFirebase />
       </Col>
     </Row>
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -4,9 +4,11 @@ import {
   SiNetlify,
   SiVisualstudiocode,
   SiPostman,
-  SiHeroku,
   SiIntellijidea,
 } from "react-icons/si";
+import {
+  DiGit
+} from "react-icons/di";
 
 function Toolstack() {
   return (
@@ -14,17 +16,17 @@ function Toolstack() {
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" title='IntelliJ IDEA'>
         <SiIntellijidea />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" title="Postman">
         <SiPostman />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons" title="Netlify">
         <SiNetlify/>
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiHeroku />
+      <Col xs={4} md={2} className="tech-icons" title="Git">
+        <DiGit />
       </Col>
     </Row>
   );
